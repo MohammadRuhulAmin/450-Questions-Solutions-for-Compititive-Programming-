3ee37c49ce5efa32b378ab06d5ac25dbd6110a2f
Md Ruhul Amin <ra.01403288711@gmail.com>
2022-02-10 08:40:13 +0600

    Kadan's Algorithm

---
+++ Array/Maximum Sum Sub Array/Maximum Sum Sub Array.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+	int length;
+	cin >> length;
+	int array[length];	
+	for(int i = 0;i<length;i++)cin >> array[i];
+	
+	
+    return 0;
+}
+
+// 1 2 3 4 5 6
+// 1 
+// 1 2
+// 1 2 3
+// 1 2 3 4
+// 1 2 3 4 5
+// 1 2 3 4 5 6
+// 2
+// 2 3
+// 2 3 4
+// 2 3 4 5
+// 2 3 4 5 6
+// 3 
+// 3 4
+// 3 4 5 
+// 3 4 5 6
+// 4 
+// 4 5
+// 4 5 6
+// 5
+// 5 6
